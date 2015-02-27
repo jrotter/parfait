753c239c0426d3aab91ccaa7e9f7a851879c5d89
Jeremy Rotter <jrotter@carekinesis.com>
2015-02-27 15:35:27 -0500

    Simple Watir example

---
+++ examples/simple_watir.rb
@@ -0,0 +1,11 @@
+require 'watir-webdriver'
+require 'webtester'
+
+browser = Watir::Browser.new
+browser.goto 'http://staging.eirenerx.com/login'
+
+# Login 
+browser.text_field(:name => 'userid').when_present.set 'MUPrescriberman'
+browser.text_field(:name => 'password').when_present.set 'Testmd678'
+browser.button(:name => 'submit').when_present.click
+browser.
